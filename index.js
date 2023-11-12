fe79c2af559e459d42490691fd5ed078b3e7bcdc
Chad Edwards <chadedwardsofficial@gmail.com>
2023-11-12 14:25:30 -0600

    added functionality for svg logo, need to add test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-
+const {Circle, Square, Triangle} = require("./lib/shapes");
 
 
 const questions = [
@@ -15,9 +15,9 @@ const questions = [
     name: 'titleColor',
   },
   {
-    type: 'input',
+    type: 'list',
     message: 'What shape would you like?',
-    name: 'shape',
+    name: 'shapeChosen',
     choices: ['Square', 'Circle', 'Triangle',],
   },
   {
@@ -26,3 +26,54 @@ const questions = [
     name: 'shapeColor',
   },
 ];
+
+function generateSVG(answers) {
+  let shape;
+
+  if (answers.shapeChosen === 'Square') {
+    shape = new Square();
+  } else if (answers.shapeChosen === 'Circle') {
+    shape = new Circle();
+  } else if (answers.shapeChosen === 'Triangle') {
+    shape = new Triangle();
+  } ;
+  
+
+  shape.setColor(answers.shapeColor);
+  shape.setText(answers.name);
+  shape.setTextcolor(answers.titleColor);
+
+  return `
+  
+  <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+
+  ${shape.create()}                                                                                                                                                   
+  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${shape.textColor}">${shape.text}</text>                                                          
+</svg>
+`;
+}
+
+
+
+
+
+
+inquirer
+  .prompt(questions)
+  .then((answers) => {
+    const svgContent = generateSVG(answers);
+
+    fs.writeFile('LOGO.svg', svgContent, (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Enjoy Your New Logo!');
+      }
+    });
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+
+
